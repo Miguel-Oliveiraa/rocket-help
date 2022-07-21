26da8e16c8be11c63160f7de936960885e3f9974
Miguel Oliveira <miguellgomes64@gmail.com>
2022-07-21 17:27:17 -0300

    feat: setting up native base and fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
 import SignIn from "./src/screens/SingIn";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
+
+import { NativeBaseProvider, StatusBar } from "native-base";
+
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
 }

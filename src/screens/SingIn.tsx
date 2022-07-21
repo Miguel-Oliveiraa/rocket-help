236bb42e2e37c27b1e37d91da6d420bb58b12bd9
Miguel Oliveira <miguellgomes64@gmail.com>
2022-07-21 18:38:44 -0300

    feat: SingIn screen finished

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,11 +1,42 @@
-import { VStack, Heading } from "native-base";
+import { useState } from "react";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
+import Logo from "../assets/logo_primary.svg";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { colors } = useTheme();
+
+  function handleSingIn() {
+    console.log(email, password);
+  }
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
       <Heading color="gray.100" fontSize="xl" marginTop={20} marginBottom={6}>
         Acessar sua conta
       </Heading>
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      <Button title="Entrar" w="full" onPress={handleSingIn} />
     </VStack>
   );
 }
